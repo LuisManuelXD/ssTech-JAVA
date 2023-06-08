d012ab539edd376af512f4a83272996bfa207adf
Luis Manuel Rios <riostorresGamer@gmail.com>
2023-06-08 00:13:31 -0600

    ST-0 Creacion de ventanas de clientes

---
+++ src/model/Client.java
@@ -0,0 +1,52 @@
+package model;
+
+public class Client {
+    private String name;
+    private String lastName;
+    private int age;
+    private String mail;
+    private String cellPhone;
+
+    public Client() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getCellPhone() {
+        return cellPhone;
+    }
+
+    public void setCellPhone(String cellPhone) {
+        this.cellPhone = cellPhone;
+    }
+}
