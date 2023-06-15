e085ddc75d614d3a00b544dbf1b0ed7c907022a6
Luis Manuel Rios <riostorresGamer@gmail.com>
2023-06-14 20:35:52 -0600

    ST-2 Se agrega icono al programa

--- src/tets/main.java
+++ src/tets/main.java
@@ -1,11 +1,15 @@
 package tets;
 
+import javax.swing.ImageIcon;
 import view.Menu;
 
 public class main {
     public static void main(String[] args) {
-        Menu j = new Menu ();
-        j.setVisible(true);
+        ImageIcon icono = new ImageIcon("src/img/logo.png");
         
+        Menu menu = new Menu();
+        menu.setIconImage(icono.getImage());
+        menu.setLocationRelativeTo(null);
+        menu.setVisible(true);
     }
 }
