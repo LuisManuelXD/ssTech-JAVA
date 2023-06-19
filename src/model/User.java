5faab92200fda083a512f8905fdb5911d7e87b6a
Luis Manuel Rios <riostorresGamer@gmail.com>
2023-06-18 19:27:35 -0600

    ST-3 Se modifica la ventana de inicio al correr el programa y se añade un costructor

--- src/model/User.java
+++ src/model/User.java
@@ -10,6 +10,11 @@ public class User {
     public User() {
     }
 
+    public User(String user, String password) {
+        this.user = user;
+        this.password = password;
+    }
+
     public String getUser() {
         return user;
     }
--- src/tets/main.java
+++ src/tets/main.java
@@ -1,15 +1,15 @@
 package tets;
 
 import javax.swing.ImageIcon;
-import view.Menu;
+import view.Login;
 
 public class main {
     public static void main(String[] args) {
         ImageIcon icono = new ImageIcon("src/img/logo.png");
         
-        Menu menu = new Menu();
-        menu.setIconImage(icono.getImage());
-        menu.setLocationRelativeTo(null);
-        menu.setVisible(true);
+        Login login = new Login();
+        login.setIconImage(icono.getImage());
+        login.setLocationRelativeTo(null);
+        login.setVisible(true);
     }
 }
